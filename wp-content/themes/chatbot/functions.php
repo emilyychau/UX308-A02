548a0c3c530f6ef0e2b0bb4d671c5703e1ee4b1c
emilyychau <112610091+emilyychau@users.noreply.github.com>
2025-03-29 02:14:51 +0000

    update wp_enqueue_script modules

--- wp-content/themes/chatbot/functions.php
+++ wp-content/themes/chatbot/functions.php
@@ -11,7 +11,8 @@ function theme_enqueue_styles() {
         array('parent-style')
     );
     wp_enqueue_script('custom_javascript', get_stylesheet_directory_uri() . '/customElements.js');
-    
+    wp_enqueue_script_module('custom_module1', "https://emilyychau.github.io/UX308-fab/index.js");
+    wp_enqueue_script_module('custom_module2', "https://emilyychau.github.io/UX308-fab/fab.js");
 }
 //
 // Your code goes below
