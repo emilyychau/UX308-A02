48dafcf384e58c6af43ee4ef1975099874c0ad98
emilyychau <112610091+emilyychau@users.noreply.github.com>
2025-03-29 02:05:02 +0000

    remove wp_enqueue_script

--- wp-content/themes/chatbot/functions.php
+++ wp-content/themes/chatbot/functions.php
@@ -11,8 +11,6 @@ function theme_enqueue_styles() {
         array('parent-style')
     );
     wp_enqueue_script('custom_javascript', get_stylesheet_directory_uri() . '/customElements.js');
-    wp_enqueue_script_module('custom_module1', "https://emilyychau.github.io/UX308-fab/index.js");
-    wp_enqueue_script_module('custom_module2', "https://emilyychau.github.io/UX308-fab/fab.js");
 }
 //
 // Your code goes below
